2ae01aae4e91ac4a96d7b9b3b4b525c6d19de547
Pranav Vashi <neobuddy89@gmail.com>
2023-05-26 00:00:42 +0530

    base: Add overlay to mock oplus LinearmotorVibratorService
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -2676,9 +2676,12 @@ public final class SystemServer implements Dumpable {
             mSystemServiceManager.startService(AttestationService.class);
             t.traceEnd();
 
-            t.traceBegin("StartLinearmotorVibratorService");
-            mSystemServiceManager.startService(LinearmotorVibratorService.class);
-            t.traceEnd();
+            if (context.getResources().getBoolean(
+                    com.android.internal.R.bool.config_mockOplusLinearmotorVibratorService)) {
+                t.traceBegin("StartLinearmotorVibratorService");
+                mSystemServiceManager.startService(LinearmotorVibratorService.class);
+                t.traceEnd();
+            }
         }
 
         t.traceBegin("StartMediaProjectionManager");
